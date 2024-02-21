e3eb1fb3bab108f2e640e0f9c43326282d837f59
akshay-kakade <akshay.kakade@builder.ai>
2024-02-21 09:51:05 +0530

    configure webmock for mocking dev request

---
+++ config/initializers/web_mock.rb
@@ -0,0 +1,14 @@
+# config/initializers/webmock.rb
+require 'webmock'
+
+if Rails.env.development?
+  WebMock.enable!
+  
+  # Define your mocks here
+
+  WebMock.stub_request(:post, "https://api.iterable.com/api/events/track")
+  .to_return(status: 200, body: "{\"data\": \"mocked_response\"}", headers: {})
+
+  WebMock.stub_request(:post, "https://api.iterable.com/api/email/target")
+         .to_return(status: 200, body: "{\"data\": \"mocked_response\"}", headers: {})
+end
